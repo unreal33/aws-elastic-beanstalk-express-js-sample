344c8be0541a9fc7a6dcaa874273544c9d7ac039
Fat Penguin <ddt19887585@aol.com>
2021-06-28 13:45:33 -0400

    New Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Andrzej, witam w Polsce!'));
+app.get('/', (req, res) => res.send('Hello Andrzej, serdeczne witam w Polsce!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
